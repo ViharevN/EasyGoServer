9a204b10da6ec953fd6914e32706867f08cd956b
ViharevN <swix_off@mail.ru>
2023-12-02 13:41:26 +0300

    add serverMux

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import "net/http"
+
+func mainPage(req http.ResponseWriter, res *http.Request) {
+	req.Write([]byte("Hello!"))
+}
+
+func apiPage(req http.ResponseWriter, res *http.Request) {
+	req.Write([]byte("This is api page/"))
+}
+
 func main() {
 
+	http.HandleFunc("/", mainPage)
+	http.HandleFunc("/api", apiPage)
+
+	err := http.ListenAndServe(`:8080`, nil)
+	if err != nil {
+		panic(err)
+	}
 }
