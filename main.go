7fdbbed5c1c414242dbc847ac8bae73cacf3a7d1
ViharevN <swix_off@mail.ru>
2023-12-02 13:16:45 +0300

    add first handler

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "net/http"
 
+type MyHandler struct {
+}
+
+func (h MyHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	data := []byte("Hello")
+	res.Write(data)
+}
+
 func main() {
 	err := http.ListenAndServe(`:8080`, nil)
 	if err != nil {
