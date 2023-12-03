07e276663e3239ee05d0d3cd8af12411c78f0071
ViharevN <swix_off@mail.ru>
2023-12-03 22:04:42 +0300

    add middleware

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+func rootHandle(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello"))
+}
+
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	http.Handle("/", middleware(http.HandlerFunc(rootHandle)))
+
+	err := http.ListenAndServe(`:8080`, nil)
+	if err != nil {
+		panic(err)
+	}
 }
