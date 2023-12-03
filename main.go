fef4ca6ef68f002710f83b1ccec13697d5fc1bbf
ViharevN <swix_off@mail.ru>
2023-12-04 01:08:31 +0300

    add testcase #3

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+type User struct {
+	FirstName string
+	LastName  string
+}
+
+func (u User) FullName() string {
+	return u.FirstName + " " + u.LastName
+}
+
+func main() {
+	u := User{
+		FirstName: "Misha",
+		LastName:  "Popov",
+	}
+	fmt.Println(u.FullName())
 }
--- main_test.go
+++ main_test.go
@@ -1,1 +1,42 @@
 package main
+
+import "testing"
+
+func TestUser_FullName(t *testing.T) {
+	tests := []struct {
+		name      string
+		firstName string
+		lastName  string
+		fullName  string
+	}{
+		{
+			name:      "testcase #1",
+			firstName: "John",
+			lastName:  "Johnson",
+			fullName:  "John Johnson",
+		}, {
+			name:      "testcase #2",
+			firstName: "Sergey",
+			lastName:  "Sergeev",
+			fullName:  "Sergey Sergeev",
+		}, {
+			name:      "testcase #3",
+			firstName: "Georgy",
+			lastName:  "Georgiev",
+			fullName:  "Georgy Georgiev",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			user := User{
+				FirstName: test.firstName,
+				LastName:  test.lastName,
+			}
+
+			if fullname := user.FullName(); fullname != test.fullName {
+				t.Errorf("Expected %s, but got %s", test.fullName, fullname)
+			}
+		})
+	}
+}
