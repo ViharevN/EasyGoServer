ace9f2e35e57cb07bf7d9a864421bee32522151b
ViharevN <swix_off@mail.ru>
2023-12-04 01:27:50 +0300

    add testcase #3

--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+type Relationship string
+
+const (
+	Father      = Relationship("father")
+	Mother      = Relationship("mother")
+	Child       = Relationship("child")
+	GrandMother = Relationship("grandMother")
+	GrandFather = Relationship("grandFather")
+)
+
+type Family struct {
+	Members map[Relationship]Person
+}
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+var (
+	ErrRelationShipAlreadyExists = errors.New("relationship allreqdy exists")
+)
+
+func (f *Family) AddNew(r Relationship, p Person) error {
+	if f.Members == nil {
+		f.Members = map[Relationship]Person{}
+	}
+	if _, ok := f.Members[r]; ok {
+		return ErrRelationShipAlreadyExists
+	}
+	f.Members[r] = p
+	return nil
+}
+
+func main() {
+	f := Family{}
+	err := f.AddNew(Father, Person{
+		FirstName: "Misha",
+		LastName:  "Popov",
+		Age:       56,
+	})
+	fmt.Println(f, err)
+
+	err = f.AddNew(Father, Person{
+		FirstName: "Grug",
+		LastName:  "Mishi",
+		Age:       57,
+	})
+	fmt.Println(f, err)
 }
--- main_test.go
+++ main_test.go
@@ -1,1 +1,42 @@
 package main
+
+import "testing"
+
+func TestFamily_AddNew(t *testing.T) {
+	tests := []struct {
+		name         string
+		relationship Relationship
+		person       Person
+		expectedErr  error
+	}{
+		{
+			name:         "testcase #1",
+			relationship: Father,
+			person: Person{
+				FirstName: "Misha",
+				LastName:  "Popov",
+				Age:       56,
+			},
+			expectedErr: nil,
+		}, {
+			name:         "testcase #2",
+			relationship: Father,
+			person: Person{
+				FirstName: "Grug",
+				LastName:  "Mishi",
+				Age:       57,
+			},
+			expectedErr: ErrRelationShipAlreadyExists,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			f := Family{}
+			err := f.AddNew(test.relationship, test.person)
+			if err != test.expectedErr {
+				t.Errorf("Expected error %v, but got %v", test.expectedErr, err)
+			}
+		})
+	}
+}
