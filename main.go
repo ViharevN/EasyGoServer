1c08824022c598ab36fdec6b2e0b68bdfcf2aabb
ViharevN <swix_off@mail.ru>
2023-12-03 22:11:26 +0300

    add redirect

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "net/http"
 
+func redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://ya.ru", http.StatusMovedPermanently)
+}
+
+func main() {
+	http.HandleFunc("/search", redirect)
+	err := http.ListenAndServe(`:8080`, nil)
+	if err != nil {
+		panic(err)
+	}
 }
