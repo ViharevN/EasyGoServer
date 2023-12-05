a4d4b236ee982ee8822cfa5b42ee8c8068213396
ViharevN <swix_off@mail.ru>
2023-12-05 23:53:51 +0300

    add methodGet, No-client-code

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
+func main() {
+	response, err := http.Get("https://practicum.yandex.ru")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	body, err := io.ReadAll(response.Body)
+	defer response.Body.Close()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i := 0; i < 512 && i < len(body); i++ {
+		fmt.Printf("%d ", body[i])
+	}
+
+	fmt.Printf("\r\nStatus code: %d\r\n", response.StatusCode)
+	for k, v := range response.Header {
+		fmt.Printf("%s: %v\r\n", k, v[0])
+	}
 }
