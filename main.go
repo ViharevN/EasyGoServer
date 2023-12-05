57566bd818bb04fe95a5ea9fed6a078f80066777
ViharevN <swix_off@mail.ru>
2023-12-06 00:58:40 +0300

    first client-request

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
 func main() {
+	client := &http.Client{}
+
+	request, err := http.NewRequest(http.MethodGet, "http://localhost:8080", http.NoBody)
+	if err != nil {
+		panic(err)
+	}
+	request.Header.Set(`MyHeader`, "Hello")
+	request.Header.Add(`MyHeader`, "Привет")
+
+	response, err := client.Do(request)
+	io.Copy(os.Stdout, response.Body)
+	defer response.Body.Close()
 
 }
