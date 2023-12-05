d725b48f99ed02c2aa6aefe9ac36596b98e371f5
ViharevN <swix_off@mail.ru>
2023-12-06 00:39:35 +0300

    check redirect

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
 func main() {
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			fmt.Println(req.URL)
+			return nil
+		},
+	}
+	response, err := client.Get("http://ya.ru")
+	if err != nil {
+		fmt.Errorf("Error: %s", err)
+	}
+
+	defer response.Body.Close()
+	if _, err = io.ReadAll(response.Body); err != nil {
+		fmt.Println(err)
+	}
+	for s, v := range response.Header {
+		fmt.Printf("%s: %v\r\n", s, v)
+	}
 
 }
