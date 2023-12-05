f361da8987682f774baa4df840f9bb818a4441bb
ViharevN <swix_off@mail.ru>
2023-12-06 01:14:14 +0300

    first post client-request

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+)
+
 func main() {
+	client := &http.Client{}
+
+	var body = []byte(`{"message":"Hello"}`)
+	request, err := http.NewRequest(http.MethodPost, "http://localhost:8080", bytes.NewBuffer(body))
+	if err != nil {
+		panic(err)
+	}
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	res, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(res))
 
 }
